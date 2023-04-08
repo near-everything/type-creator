7fd0dc7fcb4c6a15c3dc1c6b7985c63604ffbf09
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2023-04-07 22:15:05 -0400

    feature: adds more basic types

--- src/components/FormBuilder.tsx
+++ src/components/FormBuilder.tsx
@@ -1,37 +1,143 @@
 import {
   Button,
+  Checkbox,
   FormControl,
   FormLabel,
   Input,
+  NumberInput,
+  NumberInputField,
   Select,
-  VStack,
+  Textarea,
+  VStack
 } from "@chakra-ui/react";
 import { useState } from "react";
-
-interface FormElement {
-  type: string;
-  value: string;
-}
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 enum ElementTypes {
   STRING = "string",
+  NUMBER = "number",
+  BOOLEAN = "boolean",
+  DATE = "date",
+  TEXTAREA = "textarea",
+  DROPDOWN = "dropdown",
 }
 
+type Element = {
+  type: ElementTypes;
+  value: any;
+};
+
 function FormBuilder() {
-  const [formElements, setFormElements] = useState<FormElement[]>([]);
+  const [elements, setElements] = useState<Element[]>([]);
   const [selectedType, setSelectedType] = useState<ElementTypes>(
     ElementTypes.STRING
   );
 
   const handleAddElement = () => {
-    const newElement = { type: selectedType, value: '' };
-    setFormElements([...formElements, newElement]);
+    switch (selectedType) {
+      case ElementTypes.NUMBER:
+        setElements([...elements, { type: selectedType, value: 0 }]);
+        break;
+      case ElementTypes.BOOLEAN:
+        setElements([...elements, { type: selectedType, value: false }]);
+        break;
+      case ElementTypes.DATE:
+        setElements([...elements, { type: selectedType, value: new Date() }]);
+        break;
+      case ElementTypes.TEXTAREA:
+        setElements([...elements, { type: selectedType, value: "" }]);
+        break;
+      case ElementTypes.DROPDOWN:
+        setElements([...elements, { type: selectedType, value: "" }]);
+        break;
+      default:
+        setElements([...elements, { type: selectedType, value: "" }]);
+        break;
+    }
   };
-  
-  const handleChange = (index: number, value: string) => {
-    const updatedElements = [...formElements];
+
+  const handleChange = (index: number, value: any) => {
+    const updatedElements = [...elements];
     updatedElements[index].value = value;
-    setFormElements(updatedElements);
+    setElements(updatedElements);
+  };
+
+  const renderElement = (element: Element, index: number) => {
+    switch (element.type) {
+      case ElementTypes.NUMBER:
+        return (
+          <FormControl key={index}>
+            <FormLabel>Element {index + 1}</FormLabel>
+            <NumberInput
+              value={element.value}
+              onChange={(value) => handleChange(index, value)}
+            >
+              <NumberInputField />
+            </NumberInput>
+          </FormControl>
+        );
+      case ElementTypes.BOOLEAN:
+        return (
+          <FormControl key={index}>
+            <FormLabel>Element {index + 1}</FormLabel>
+            <Checkbox
+              isChecked={element.value}
+              onChange={(e) => handleChange(index, e.target.checked)}
+            >
+              Value
+            </Checkbox>
+          </FormControl>
+        );
+      case ElementTypes.DATE:
+        return (
+          <FormControl key={index}>
+            <FormLabel>Element {index + 1}</FormLabel>
+            <DatePicker
+              selected={element.value}
+              onChange={(value) => handleChange(index, value)}
+            />
+          </FormControl>
+        );
+      case ElementTypes.TEXTAREA:
+        return (
+          <FormControl key={index}>
+            <FormLabel>Element {index + 1}</FormLabel>
+            <Textarea
+              value={element.value}
+              onChange={(e) => handleChange(index, e.target.value)}
+            />
+          </FormControl>
+        );
+      case ElementTypes.DROPDOWN:
+        return (
+          <FormControl key={index}>
+            <FormLabel>Element {index + 1}</FormLabel>
+            <Select
+              value={element.value}
+              onChange={(e) => handleChange(index, e.target.value)}
+            >
+              <option value="">Select an option</option>
+              <option value="Option 1">Option 1</option>
+              <option value="Option 2">Option 2</option>
+              <option value="Option 3">Option 3</option>
+            </Select>
+          </FormControl>
+        );
+      case ElementTypes.STRING:
+        return (
+          <FormControl key={index}>
+            <FormLabel>String Input</FormLabel>
+            <Input
+              type="text"
+              value={element.value as string}
+              onChange={(e) => handleChange(index, e.target.value)}
+            />
+          </FormControl>
+        );
+      default:
+        return null;
+    }
   };
 
   return (
@@ -43,30 +149,15 @@ function FormBuilder() {
           onChange={(e) => setSelectedType(e.target.value as ElementTypes)}
         >
           <option value={ElementTypes.STRING}>String</option>
-          {/* add options for other element types */}
+          <option value={ElementTypes.NUMBER}>Number</option>
+          <option value={ElementTypes.BOOLEAN}>Boolean</option>
+          <option value={ElementTypes.DATE}>Date</option>
+          <option value={ElementTypes.TEXTAREA}>Text Area</option>
+          <option value={ElementTypes.DROPDOWN}>Dropdown</option>
         </Select>
       </FormControl>
       <Button onClick={handleAddElement}>Add {selectedType} Input</Button>
-      <form>
-        {formElements.map((element, index) => {
-          switch (element.type) {
-            case ElementTypes.STRING:
-              return (
-                <FormControl key={index}>
-                  <FormLabel>String Input</FormLabel>
-                  <Input
-                    type="text"
-                    value={element.value}
-                    onChange={(e) => handleChange(index, e.target.value)}
-                  />
-                </FormControl>
-              );
-            // add more cases for other element types
-            default:
-              return null;
-          }
-        })}
-      </form>
+      {elements.map((element, index) => renderElement(element, index))}
     </VStack>
   );
 }
